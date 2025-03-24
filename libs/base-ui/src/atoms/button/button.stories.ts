fda6f1fed5a064839956c168168bed1382019674
Thachdong <thachdong270293@gmail.com>
2025-03-24 22:42:46 +0700

    config(storybook): set up storybook

---
+++ libs/base-ui/src/atoms/button/button.stories.ts
@@ -0,0 +1,12 @@
+import { Button } from '.';
+
+export default {
+  title: 'Atoms/Button',
+  component: Button,
+  tags: ['autodocs'],
+  args: {
+    children: 'Button',
+  }
+};
+
+export const Default = {}
