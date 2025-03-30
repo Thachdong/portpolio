7b1ee4a795cd6e1cd49037df2a7f513177f51fa6
Thachdong <thachdong270293@gmail.com>
2025-03-30 12:57:11 +0700

    feat(blog): add page blog detail

--- apps/blog/src/app/blog/[categoryId]/[postId]/page.tsx
+++ apps/blog/src/app/blog/[categoryId]/[postId]/page.tsx
@@ -1,7 +1,7 @@
-import { PostComments, PostDetail, SameCategoryPosts } from '@/blog-ui';
-import { getMdFileContent, getPostByIdService } from '@/database';
-import { serialize } from 'next-mdx-remote/serialize';
-import matter from 'gray-matter';
+import { Breadcrumb, PostComments, SameCategoryPosts } from '@/blog-ui';
+import { getPostByIdService, getMdFileContent, TFrontMatter } from '@/database';
+import { compileMDX } from 'next-mdx-remote/rsc';
+import { markdownComponents } from '@/utility';
 
 type TPostDetailProps = {
   params: {
@@ -12,32 +12,57 @@ type TPostDetailProps = {
 
 const CLASSNAMES = {
   container: 'grid grid-cols-[1fr,275px] gap-x-4 h-full',
-  content: 'flex flex-col gap-y-12',
+  content: 'flex flex-col gap-y-4',
 };
 
 export default async function PostDetailPage({
-  params: { postId },
+  params,
 }: Readonly<TPostDetailProps>) {
+  const { postId } = await params;
+
   const post = await getPostByIdService(postId);
 
+  // #region -- get post content from s3
   const text = await getMdFileContent(post?.filename || '');
 
-  const { content } = matter(text || '');
-
-  const mdxContent = await serialize(content || '');
+  const { content, frontmatter } = await compileMDX<TFrontMatter>({
+    source: text || '',
+    options: {
+      parseFrontmatter: true,
+    },
+    components: markdownComponents,
+  });
+  // #endregion
 
   return (
     <div className={CLASSNAMES.container}>
       <div className={CLASSNAMES.content}>
-        <PostDetail
-          postId={postId}
-          postTitle={post?.title || ''}
-          content={mdxContent}
-          categoryId={post?.category.id || ''}
-          categoryGroupId={post?.categoryGroup.id || ''}
-          categoryName={post?.category.name || ''}
-          categoryGroupName={post?.categoryGroup.name || ''}
+        <Breadcrumb
+          items={[
+            {
+              title: post?.categoryGroup.name || '',
+              href: `/blog#${post?.categoryGroup.id}`,
+              id: post?.categoryGroup.id || '',
+            },
+            {
+              title: post?.category.name || '',
+              href: `/blog/${post?.category.id}`,
+              id: post?.category.id || '',
+            },
+            { title: post?.title || '', id: postId },
+          ]}
         />
+
+        <h1 className="text-2xl font-bold text-center capitalize">
+          {post?.title || ''}
+        </h1>
+
+        <p className="text-sm text-gray-500 border-b border-gray-300 pb-4">
+          Publish at:{' '}
+          <span className="font-bold">{frontmatter?.publishedAt}</span>
+        </p>
+
+        <div style={{ all: 'unset' }}>{content}</div>
 
         <PostComments />
       </div>
