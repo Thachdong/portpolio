05ecb6d3d783a360c612a7790f59038c1fc009a4
Thachdong <thachdong270293@gmail.com>
2025-04-01 23:34:51 +0700

    feat(blog): add feature search

--- libs/blog-ui/src/organisms/category-group-list/index.tsx
+++ libs/blog-ui/src/organisms/category-group-list/index.tsx
@@ -1,9 +1,12 @@
-import { TCategoryGroupWithCategories } from '@/database';
+import { TAdminPost, TCategoryGroupWithCategories } from '@/database';
 import { CategoryPostsList } from '../category-posts-list';
 import Link from 'next/link';
+import { PostCard } from '@/blog-ui';
 
 type TCategoryGroupListProps = {
   categoryGroups: TCategoryGroupWithCategories[];
+  searchPosts: TAdminPost[];
+  searchTerm?: string;
 };
 
 const CLASSNAMES = {
@@ -11,11 +14,42 @@ const CLASSNAMES = {
   header: 'flex justify-between items-center mb-4 border-b-2 border-[#2f6e91]',
   headerLink: 'text-[#2f6e91]',
   headerText: 'font-bold text-xl',
+  searchContainer: 'p-4',
+  searchList: 'grid grid-cols-4 gap-4',
 };
 
 export async function CategoryGroupList({
   categoryGroups,
+  searchPosts,
+  searchTerm,
 }: Readonly<TCategoryGroupListProps>) {
+  if (searchPosts && searchTerm) {
+    return (
+      <div className={CLASSNAMES.searchContainer}>
+        <p className="text-gray-500">
+          Searching for <span className="font-bold">{searchTerm}</span>
+        </p>
+        <p className="text-gray-500">
+          Results: <span className="font-bold">{searchPosts.length}</span>
+        </p>
+        <hr className="my-4 border-gray-300" />
+        <ul className={CLASSNAMES.searchList}>
+          {searchPosts.map((post) => {
+            return (
+              <PostCard
+                key={post.id}
+                id={post.id}
+                category={post.category.name}
+                title={post.title}
+                displayImage={post.category.image || ''}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <ul className={CLASSNAMES.container}>
       {categoryGroups.map((group) => {
